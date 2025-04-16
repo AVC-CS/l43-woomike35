41549507eba3319a32e29ea14e590ab299c6b727
woomike35 <michaeleze226@gmail.com>
2025-04-16 12:51:00 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 def main():
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
     total = 0
+    numbers = [0] * 5
+    i = 0
 
-    ########################################
+    while i < len(numbers):
+        numbers[i] = int(input('Enter a value: '))
+        i += 1
+
+    total = sum(numbers)
+    print(total)
+
     # Do not delete the return statement
-    ########################################
-    return total
+    return total, numbers
 
 
 if __name__ == '__main__':
